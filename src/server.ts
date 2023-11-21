4ac72abed82be03ba9928e1b77e0218447f17d54
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-11-21 23:43:52 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app';
+import config from './config';
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.DATABASE_URL as string);
+    app.listen(config.PORT, () => {
+      console.log(`The server is running at http://localhost:${config.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
